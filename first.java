8697f90d511c7c7ff69fca8fa1f266aca000731b
Ronti0005 <rontisingh24012007@gmail.com>
2025-01-15 23:14:46 +0530

    adding the first java program

---
+++ first.java
@@ -0,0 +1,12 @@
+package Java;
+
+public class first {
+    public static void main(String[] args) {
+        int i;
+        for (i=0;i<=9;i++)
+        {
+
+            System.out.println("Hello, World!");
+        }
+    }
+}
