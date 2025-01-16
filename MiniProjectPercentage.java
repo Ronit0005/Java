1816a1450cb6ed6b00a02fb9130baa022b32311e
Ronti0005 <rontisingh24012007@gmail.com>
2025-01-16 14:45:07 +0530

    adding the firs mini project

---
+++ MiniProjectPercentage.java
@@ -0,0 +1,18 @@
+package Java;
+import java.util.*;
+public class MiniProjectPercentage {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the name of the student");
+        System.out.println("Enter the marks of the student in all the 5 subjects one by one");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int d=sc.nextInt();
+        int e=sc.nextInt();
+        double average=(a+b+c+d+e)/5.0;
+        System.out.println("The average of all the 5 subjects are "+average);
+        sc.close();
+    }
+    
+}
