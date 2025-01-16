570c900328c2d0a1ed692bca9f224d4670e9d568
Ronti0005 <rontisingh24012007@gmail.com>
2025-01-16 19:27:45 +0530

    Adding the EscapeSequence file

---
+++ EscapeSequence.java
@@ -0,0 +1,9 @@
+package Java;
+
+public class EscapeSequence {
+    public static void main(String[] args){
+        //System.out.println("Hii I am Ronit\'s friend");
+        //System.out.println("Hii I am Ronit\tI am a coder");
+        System.out.println("Hii \"WELCOME\" to the world of coding");
+    }    
+}
