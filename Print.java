23c9e2c28f6cf08d0c2cd227cba970ad5e17cb61
Ronti0005 <rontisingh24012007@gmail.com>
2025-01-16 18:48:03 +0530

    editing the print file

--- Print.java
+++ Print.java
@@ -2,12 +2,12 @@ package Java;
 
 public class Print {
     public static void main(String[] args){
-        System.out.print("Hii world ");
-        System.out.println("Hello world");
-        int a=1;double d =10.01;
-        System.out.printf("The value of a is %d and that of d is %.2f",a,d);
+        //System.out.print("Hii world ");
+        //System.out.println("Hello world");
+        //int a=1;double d =10.01;
+        //System.out.printf("The value of a is %d and that of d is %.2f",a,d);
         float c =90.0956f;double e=98.354;
-        System.out.format("The value of c is %f and that of e is %f",c,e);
+        System.out.format("The value of c is %f and that of e is %10.2f",c,e);
     }
     
 }
