2adc8f7f899de0a179fa4e74fedb3da26659cf4d
Ronti0005 <rontisingh24012007@gmail.com>
2025-01-16 19:14:08 +0530

    adding a file of string methods

---
+++ StringsMethods.java
@@ -0,0 +1,24 @@
+package Java;
+
+public class StringsMethods {
+    public static void main(String[] args){
+        String name="Ronit";
+        /*int length=name.length();
+        System.err.println(length);*/
+        //System.out.println(name.toLowerCase());
+        //System.out.println(name.toUpperCase());
+        //System.out.println(name.trim());
+        //System.out.println(name.substring(2));
+        //System.out.println(name.substring(2,4));
+        //System.out.println(name.replace('n', 'h'));
+        //System.out.println(name.startsWith("Ro"));
+        //System.out.println(name.endsWith("it"));
+        //System.out.println(name.charAt(0));
+        //System.out.println(name.indexOf("R"));
+        //System.out.println(name.indexOf('i', 2));
+        //System.out.println(name.lastIndexOf("it"));
+        //System.out.println(name.equals("Roni"));
+        System.out.println(name.equalsIgnoreCase("RoNiT"));
+    }
+    
+}
