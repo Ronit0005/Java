ac61dc172a7863a48dc00bff9a68927ebcc028e3
Ronti0005 <rontisingh24012007@gmail.com>
2025-01-16 14:57:57 +0530

    adding a mini project

---
+++ MiniProjectCheck.java
@@ -0,0 +1,20 @@
+package Java;
+
+import java.util.Scanner;
+
+public class MiniProjectCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello World");
+        System.out.println("Enter the number to be checked whether it is a integer or not");
+        boolean a =sc.hasNextInt();
+        if (a){
+            System.out.println("The entered number is a integer");
+        }
+        else{
+            System.out.println("The entered number is not a integer");
+        }
+        sc.close();
+    }
+    
+}
