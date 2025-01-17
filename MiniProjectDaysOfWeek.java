18079504f38d11a683e9ae4d32982392e8753fe4
Ronti0005 <rontisingh24012007@gmail.com>
2025-01-17 14:40:39 +0530

    Editing the mini project Days of week

--- MiniProjectDaysOfWeek.java
+++ MiniProjectDaysOfWeek.java
@@ -3,15 +3,19 @@ import java.util.*;
 public class MiniProjectDaysOfWeek {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of the day of the week");
         int a=sc.nextInt();
         switch (a) {
-            case 1:
-                
-    
-        
-            default:
-                break;
+            case 1->System.out.println("Today is Monday");
+            case 2->System.out.println("Today is Tuesday");
+            case 3->System.out.println("Today is Wednesday");
+            case 4->System.out.println("Today is Thursday");
+            case 5->System.out.println("Today is Friday");
+            case 6->System.out.println("Today is Saturday");
+            case 7->System.out.println("Today is Sunday");
+            default->System.out.println("Invalid Input");
         }
+        sc.close();
     }
     
 }
