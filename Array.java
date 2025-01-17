a28a12f0d7ac7e114b97bd879025fa2c364cb2fb
Ronti0005 <rontisingh24012007@gmail.com>
2025-01-17 15:25:11 +0530

    Adding the array file in repo

---
+++ Array.java
@@ -0,0 +1,17 @@
+package Java;
+import java.util.Scanner;
+public class Array {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        //int [] arr;
+        //arr=new int[5];
+        int[] arr=new int[5];
+        System.out.println("Enter the elements of array one by one");
+        for(int i=0;i<arr.length;i++){
+          arr[i]=sc.nextInt();  
+        }
+        for(int a:arr){
+            System.out.println(a);// For each loop in java.
+        }
+    }
+}
