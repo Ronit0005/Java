37ecf25fee69d90aa5670b860161cbc4481c753e
Ronti0005 <rontisingh24012007@gmail.com>
2025-01-21 23:55:59 +0530

    Adding the constructor file to the repo

---
+++ Constructor.java
@@ -0,0 +1,13 @@
+package Java;
+import java.util.*;
+class Employee02{
+    int id,salary;String name;
+    Employee02(){
+        System.out.println("The object is created");
+    }
+}
+public class Constructor {
+    public static void main(String[] args){
+      Employee02 ronit=new Employee02();
+    }
+}
