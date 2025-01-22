90dcc24fdbf349df8cea7c7acc87488c145684a9
Ronti0005 <rontisingh24012007@gmail.com>
2025-01-22 20:11:02 +0530

    Adding the dynamic method dispatch file to the repo

---
+++ DynamicMethodDispatch.java
@@ -0,0 +1,25 @@
+package Java;
+class Phone{
+    public void calling(){
+        System.out.println("Calling anyone......");
+    }
+    public void radio(){
+        System.out.println("Radio is on....");
+    }
+}
+class SmartPhone extends Phone{
+    public void calling(){
+        System.out.println("Calling as well as video calling a phone");
+    }
+    public void playingBgmi(){
+        System.out.println("Playing bgmi......");
+    }
+}
+public class DynamicMethodDispatch {
+    public static void main(String[] args){
+        Phone ob=new SmartPhone();
+        ob.radio();
+        //ob.playingBgmi(); // Throws an error.
+        ob.calling();
+    }
+}
